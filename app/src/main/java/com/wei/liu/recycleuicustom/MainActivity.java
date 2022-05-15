b5d3a9d7060a747afb9ee785be26a81b96ab8ae8
wei.liu <wei.liu@thundersoft.com>
2022-05-15 16:44:31 +0800

    Custom Recycel View  UI.

---
+++ app/src/main/java/com/wei/liu/recycleuicustom/MainActivity.java
@@ -0,0 +1,44 @@
+package com.wei.liu.recycleuicustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Star> starList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new StarAdapter(this, starList));
+
+        // 添加自定义ItemDecoration
+        recyclerView.addItemDecoration(new StarDecoration(this));
+    }
+
+    private void init() {
+        starList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i % 2 == 0) {
+                    starList.add(new Star("何炅" + j, "快乐家族" + i));
+                } else {
+                    starList.add(new Star("汪涵" + j, "天天兄弟" + i));
+                }
+            }
+        }
+    }
+}
